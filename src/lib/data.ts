245e281e767d4122ec572383505d745935550bea
ObesePanda <nava.saidalfredo@gmail.com>
2024-11-20 15:41:10 -0600

    Creacion SideMenuCard

---
+++ src/lib/data.ts
@@ -0,0 +1,321 @@
+import { colors } from "./colors";
+
+export interface Playlist {
+  id: string;
+  albumId: number;
+  title: string;
+  color: (typeof colors)[keyof typeof colors];
+  cover: string;
+  artists: string[];
+}
+
+export const playlists: Playlist[] = [
+  {
+    id: '1',
+    albumId: 1,
+    title: "Whiplash-The 5th Mini Album",
+    color: colors.yellow,
+    cover:
+      "https://cdn-images.dzcdn.net/images/cover/bb3517be9700d1e9ae0451e2186eee25/0x1900-000000-80-0-0.jpg",
+    artists: ["Aespa"],
+  },
+  {
+    id: '2',
+    albumId: 2,
+    title: "BORN PINK",
+    color: colors.green,
+    cover:
+      "https://cdn-images.dzcdn.net/images/cover/b1d3e09da20c39a3132269f5a52a4a1d/0x1900-000000-80-0-0.jpg",
+    artists: ["BLACKPINK"],
+  },
+  {
+    id: '3',
+    albumId: 3,
+    title: "APT.",
+    color: colors.rose,
+    cover:
+      "https://cdn-images.dzcdn.net/images/cover/258e6042338ce64bb4157c0c94b232ac/0x1900-000000-80-0-0.jpg",
+    artists: ["ROSÉ", "Bruno Mars"],
+  },
+  {
+    id: '4',
+    albumId: 4,
+    title: "Blue Note Study Time",
+    color: colors.blue,
+    cover:
+      "https://f4.bcbits.com/img/a1962013209_16.jpg",
+    artists: ["Raimu", "Yasumu"],
+  },
+  {
+    id: '5',
+    albumId: 5,
+    title: "Chau Saura Session",
+    color: colors.purple,
+    cover:
+      "https://f4.bcbits.com/img/a2793859494_16.jpg",
+    artists: ["Chau Saura", "amies", "kyu"],
+  },
+  {
+    id: '6',
+    albumId: 6,
+    title: "Like a Necessity",
+    color: colors.orange,
+    cover:
+      "https://f4.bcbits.com/img/a0363730459_16.jpg",
+    artists: ["WFS", "Nadav Cohen"],
+  },
+];
+
+export const morePlaylists = playlists.map((item) => ({
+  ...item,
+  id: item.id + "_more",
+}))
+
+export const sidebarPlaylists = playlists.map((item) => ({
+  ...item,
+  id: item.id + "_side",
+}))
+
+export const allPlaylists = [
+  ...playlists,
+  ...morePlaylists,
+  ...sidebarPlaylists,
+]
+
+export interface Song {
+  id: number;
+  albumId: number;
+  title: string;
+  image: string;
+  artists: string[];
+  album: string;
+  duration: string;
+}
+
+export const songs: Song[] = [
+  {
+    "id": 1,
+    "albumId": 1,
+    "title": "Moonlit Walk",
+    "image": `https://vinyl.lofirecords.com/cdn/shop/products/VINYL_MORNING_COFFEE_4-min.png?v=1680526353`,
+    "artists": ["LoFi Dreamer"],
+    "album": "Chill Lo-Fi Music",
+    "duration": "3:12"
+  },
+  {
+    "id": 2,
+    "albumId": 1,
+    "title": "Coffee Daze",
+    "image": `https://vinyl.lofirecords.com/cdn/shop/products/VINYL_MORNING_COFFEE_4-min.png?v=1680526353`,
+    "artists": ["LoFi Dreamer"],
+    "album": "Chill Lo-Fi Music",
+    "duration": "4:07"
+  },
+  {
+    "id": 3,
+    "albumId": 1,
+    "title": "Skyline Serenade",
+    "image": `https://vinyl.lofirecords.com/cdn/shop/products/VINYL_MORNING_COFFEE_4-min.png?v=1680526353`,
+    "artists": ["LoFi Dreamer"],
+    "album": "Chill Lo-Fi Music",
+    "duration": "3:50"
+  },
+  {
+    "id": 4,
+    "albumId": 1,
+    "title": "Urban Echoes",
+    "image": `https://vinyl.lofirecords.com/cdn/shop/products/VINYL_MORNING_COFFEE_4-min.png?v=1680526353`,
+    "artists": ["LoFi Dreamer"],
+    "album": "Chill Lo-Fi Music",
+    "duration": "3:30"
+  },
+  {
+    "id": 5,
+    "albumId": 1,
+    "title": "Night's End",
+    "image": `https://vinyl.lofirecords.com/cdn/shop/products/VINYL_MORNING_COFFEE_4-min.png?v=1680526353`,
+    "artists": ["LoFi Dreamer"],
+    "album": "Chill Lo-Fi Music",
+    "duration": "4:20"
+  },
+  {
+    "id": 1,
+    "albumId": 2,
+    "title": "Silent Rain",
+    "image": `https://vinyl.lofirecords.com/cdn/shop/files/2amsynth-vinyl.png?v=1693312187`,
+    "artists": ["Urban Nocturne"],
+    "album": "Midnight Tales",
+    "duration": "3:40"
+  },
+  {
+    "id": 2,
+    "albumId": 2,
+    "title": "Lost Pages",
+    "image": `https://vinyl.lofirecords.com/cdn/shop/files/2amsynth-vinyl.png?v=1693312187`,
+    "artists": ["Urban Nocturne"],
+    "album": "Midnight Tales",
+    "duration": "3:20"
+  },
+  {
+    "id": 3,
+    "albumId": 2,
+    "title": "Midnight Tales",
+    "image": `https://vinyl.lofirecords.com/cdn/shop/files/2amsynth-vinyl.png?v=1693312187`,
+    "artists": ["Urban Nocturne"],
+    "album": "Midnight Tales",
+    "duration": "3:50"
+  },
+  {
+    "id": 4,
+    "albumId": 2,
+    "title": "City Lights",
+    "image": `https://vinyl.lofirecords.com/cdn/shop/files/2amsynth-vinyl.png?v=1693312187`,
+    "artists": ["Urban Nocturne"],
+    "album": "Midnight Tales",
+    "duration": "3:30"
+  },
+  {
+    "id": 5,
+    "albumId": 2,
+    "title": "Night Drive",
+    "image": `https://vinyl.lofirecords.com/cdn/shop/files/2amsynth-vinyl.png?v=1693312187`,
+    "artists": ["Urban Nocturne"],
+    "album": "Midnight Tales",
+    "duration": "4:20"
+  },
+  {
+    "id": 1,
+    "albumId": 3,
+    "title": "Lunar",
+    "image": `https://f4.bcbits.com/img/a1435058381_65.jpg`,
+    "artists": ["Tenno"],
+    "album": "Study Session",
+    "duration": "3:40"
+  },
+  {
+    "id": 2,
+    "albumId": 3,
+    "title": "Go go go!",
+    "image": `https://f4.bcbits.com/img/a1435058381_65.jpg`,
+    "artists": ["Tenno"],
+    "album": "Study Session",
+    "duration": "3:20"
+  },
+  {
+    "id": 3,
+    "albumId": 3,
+    "title": "Keep focus!",
+    "image": `https://f4.bcbits.com/img/a1435058381_65.jpg`,
+    "artists": ["Tenno"],
+    "album": "Study Session",
+    "duration": "2:40"
+  },
+  {
+    "id": 4,
+    "albumId": 3,
+    "title": "JavaScript is the way",
+    "image": `https://f4.bcbits.com/img/a1435058381_65.jpg`,
+    "artists": ["Tenno"],
+    "album": "Study Session",
+    "duration": "3:10"
+  },
+  {
+    "id": 5,
+    "albumId": 3,
+    "title": "No more TypeScript for me",
+    "image": `https://f4.bcbits.com/img/a1435058381_65.jpg`,
+    "artists": ["Tenno"],
+    "album": "Study Session",
+    "duration": "2:10"
+  },
+  {
+    "id": 1,
+    "albumId": 4,
+    "title": "Lunar",
+    "image": "https://f4.bcbits.com/img/a1962013209_16.jpg",
+    "artists": ["Tenno"],
+    "album": "Study Session",
+    "duration": "3:40"
+  },
+  {
+    "id": 2,
+    "albumId": 4,
+    "title": "Go go go!",
+    "image": "https://f4.bcbits.com/img/a1962013209_16.jpg",
+    "artists": ["Tenno"],
+    "album": "Study Session",
+    "duration": "3:20"
+  },
+  {
+    "id": 3,
+    "albumId": 4,
+    "title": "Keep focus!",
+    "image": "https://f4.bcbits.com/img/a1962013209_16.jpg",
+    "artists": ["Tenno"],
+    "album": "Study Session",
+    "duration": "2:40"
+  },
+  {
+    "id": 4,
+    "albumId": 4,
+    "title": "JavaScript is the way",
+    "image": "https://f4.bcbits.com/img/a1962013209_16.jpg",
+    "artists": ["Tenno"],
+    "album": "Study Session",
+    "duration": "3:10"
+  },
+  {
+    "id": 5,
+    "albumId": 4,
+    "title": "No more TypeScript for me",
+    "image": "https://f4.bcbits.com/img/a1962013209_16.jpg",
+    "artists": ["Tenno"],
+    "album": "Study Session",
+    "duration": "2:10"
+  },
+  {
+    "id": 1,
+    "albumId": 5,
+    "title": "Moonlit Walk",
+    "image": "https://f4.bcbits.com/img/a2793859494_16.jpg",
+    "artists": ["LoFi Dreamer"],
+    "album": "Chill Lo-Fi Music",
+    "duration": "3:12"
+  },
+  {
+    "id": 2,
+    "albumId": 5,
+    "title": "Coffee Daze",
+    "image": "https://f4.bcbits.com/img/a2793859494_16.jpg",
+    "artists": ["LoFi Dreamer"],
+    "album": "Chill Lo-Fi Music",
+    "duration": "4:07"
+  },
+  {
+    "id": 3,
+    "albumId": 5,
+    "title": "Skyline Serenade",
+    "image": "https://f4.bcbits.com/img/a2793859494_16.jpg",
+    "artists": ["LoFi Dreamer"],
+    "album": "Chill Lo-Fi Music",
+    "duration": "3:50"
+  },
+  {
+    "id": 4,
+    "albumId": 5,
+    "title": "Urban Echoes",
+    "image": "https://f4.bcbits.com/img/a2793859494_16.jpg",
+    "artists": ["LoFi Dreamer"],
+    "album": "Chill Lo-Fi Music",
+    "duration": "3:30"
+  },
+  {
+    "id": 5,
+    "albumId": 5,
+    "title": "Night's End",
+    "image": "https://f4.bcbits.com/img/a2793859494_16.jpg",
+    "artists": ["LoFi Dreamer"],
+    "album": "Chill Lo-Fi Music",
+    "duration": "4:20"
+  },
+]
